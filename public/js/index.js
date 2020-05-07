7f7389c34ae44d8f7f853d9e87b7efc6ec2fb335
Siwei Xu <xus2@rose-hulman.edu>
2020-05-07 12:35:44 -0400

    added postPiece.html

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,7 @@ function redirect(addr) {
 
 var view1 = document.getElementById("view1");
 var view2 = document.getElementById("view2");
+var search = document.getElementById("srch");
 view1.onclick = function viewPiece() {
     location.href = "piece.html"
 }
